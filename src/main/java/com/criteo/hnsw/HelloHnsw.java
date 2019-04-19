7d4e9bc145e76fdf1e62d4492daabbd11c4f1ae2
2ooom <dmitry.parfenchik@gmail.com>
2019-04-19 12:01:38 +0200

    Minor changes in HelloHnsw

--- src/main/java/com/criteo/hnsw/HelloHnsw.java
+++ src/main/java/com/criteo/hnsw/HelloHnsw.java
@@ -1,11 +1,18 @@
 package com.criteo.hnsw;
 
+
 import java.io.File;
+import java.io.IOException;
 
 public class HelloHnsw {
-    public static void main(String[] args) {
+    // TOOD: Move to examples folder
+    public static void main(String[] args) throws IOException {
+        hnsw();
+    }
+
+    public static void hnsw() {
         int dimension = 100;
-        int nbItems = 12;
+        int nbItems = 15000;
         float seedValue = 0.5f;
         int M = 16;
         int efConstruction = 200;
@@ -15,13 +22,12 @@ public class HelloHnsw {
 
         String path = new File(".", "index-12.hnsw").toString();
 
+        System.out.println("Starting to build index");
         index.initNewIndex(nbItems, M, efConstruction);
         for (int i = 0; i < nbItems; i++) {
-            long id = i - 5;
             float[] vector = getVector(dimension, seedValue / (i + 1));
-            index.addItem(vector, id);
-            System.out.println("Set item: " + id);
-
+            index.addItem(vector, i);
+            //System.out.println("Set item: " + i);
         }
 
         System.out.println("Saving " + index.getNbItems() + " items");
@@ -32,15 +38,16 @@ public class HelloHnsw {
         long[] ids = index.getIds();
 
         for (long id : ids) {
-            System.out.println("Get item: " + id + "; First: " + index.getItem(id)[0]);
+            //System.out.println("Get item: " + id + "; First: " + index.getItem(id)[0]);
         }
 
-        long queryId = -1;
-        float[] query = index.getItem(queryId);
-        KnnResult[] results = index.knnQuery(query, 3);
-
-        for (KnnResult result : results) {
-            System.out.println(queryId + " -> " + result.getItem() + " distance: " + result.getDistance());
+        for (int i = 0; i < 20; i++) {
+            long queryId = i;
+            float[] query = index.getItem(queryId);
+            KnnResult[] results = index.knnQuery(query, 3);
+            for (KnnResult result : results) {
+                System.out.println(queryId + " -> " + result.getItem() + " distance: " + result.getDistance());
+            }
         }
         index.unload();
     }
