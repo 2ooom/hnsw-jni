174d03d1b993a071bd971a636c9e6811fe649df5
2ooom <dmitry.parfenchik@gmail.com>
2019-04-19 12:39:50 +0200

    Updating README with example

--- README.md
+++ README.md
@@ -8,4 +8,71 @@ The intent is provide fast hnsw implmentation for JVM.
 gradle init
 ./gradlew generateLib
 ```
-Will generate JNI bindings in Java and C and compile them for the current plutform.
+Will generate JNI bindings in Java and C and compile them for the current platform.
+
+
+## Usage
+Here is example that creates 15k embeddings (size 100), saving them into the index on disk, reading the index and retrieving some distances.
+
+```java
+
+import java.io.File;
+import java.io.IOException;
+
+public class HelloHnsw {
+
+    public static void main(String[] args) throws IOException {
+        int dimension = 100;
+        int nbItems = 15000;
+        float seedValue = 0.5f;
+        int M = 16;
+        int efConstruction = 200;
+
+        HnswIndex index = HnswIndex.create(Metrics.Euclidean, dimension);
+
+        index.initNewIndex(nbItems, M, efConstruction);
+        for (int i = 0; i < nbItems; i++) {
+            float[] vector = getVector(dimension, seedValue / (i + 1));
+            index.addItem(vector, i);
+        }
+
+        String path = new File("./bin", "index.hnsw").toString();
+        index.save(path);
+
+        index.load(path);
+        System.out.println("Loaded" + index.getNbItems() + " items");
+        long[] ids = index.getIds();
+
+        for (int i = 0; i < 20; i++) {
+            long queryId = ids[i];
+            float[] query = index.getItem(queryId);
+            KnnResult[] results = index.knnQuery(query, 3);
+            for (KnnResult result : results) {
+                System.out.println(queryId + " -> " + result.getItem() + " distance: " + result.getDistance());
+            }
+        }
+        index.unload();
+    }
+
+
+    private static float[] getVector(int dimension, float value) {
+        float[] vector = new float[dimension];
+        for(int i = 0; i < dimension; i++) {
+            vector[i] = value;
+        }
+        return vector;
+    }
+}
+```
+
+To run same example from repo:
+
+```
+./gradlew run
+```
+
+To run tests:
+
+```
+./gradlew test
+```
--- src/main/java/com/criteo/hnsw/HelloHnsw.java
+++ src/main/java/com/criteo/hnsw/HelloHnsw.java
@@ -1,48 +1,35 @@
 package com.criteo.hnsw;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class HelloHnsw {
-    // TOOD: Move to examples folder
+    public static Random r = new Random();
     public static void main(String[] args) throws IOException {
-        hnsw();
-    }
-
-    public static void hnsw() {
         int dimension = 100;
         int nbItems = 15000;
-        float seedValue = 0.5f;
         int M = 16;
         int efConstruction = 200;
 
-
         HnswIndex index = HnswIndex.create(Metrics.Euclidean, dimension);
 
-        String path = new File(".", "index-12.hnsw").toString();
-
-        System.out.println("Starting to build index");
         index.initNewIndex(nbItems, M, efConstruction);
         for (int i = 0; i < nbItems; i++) {
-            float[] vector = getVector(dimension, seedValue / (i + 1));
+            float[] vector = getRandomVector(dimension);
             index.addItem(vector, i);
-            //System.out.println("Set item: " + i);
         }
 
-        System.out.println("Saving " + index.getNbItems() + " items");
+        String path = new File("./bin", "index.hnsw").toString();
+
         index.save(path);
 
         index.load(path);
         System.out.println("Loaded" + index.getNbItems() + " items");
         long[] ids = index.getIds();
 
-        for (long id : ids) {
-            //System.out.println("Get item: " + id + "; First: " + index.getItem(id)[0]);
-        }
-
         for (int i = 0; i < 20; i++) {
-            long queryId = i;
+            long queryId = ids[i];
             float[] query = index.getItem(queryId);
             KnnResult[] results = index.knnQuery(query, 3);
             for (KnnResult result : results) {
@@ -52,11 +39,10 @@ public class HelloHnsw {
         index.unload();
     }
 
-
-    private static float[] getVector(int dimension, float value) {
+    public static float[] getRandomVector(int dimension) {
         float[] vector = new float[dimension];
         for(int i = 0; i < dimension; i++) {
-            vector[i] = value;
+            vector[i] = r.nextFloat();
         }
         return vector;
     }
